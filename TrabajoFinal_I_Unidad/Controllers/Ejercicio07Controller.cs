d428c3b76fdde4f97e424522f8422b32c9c1a0ce
Christian <christianrogerpm@gmail.com>
2022-04-24 23:33:13 -0500

    edit

---
+++ TrabajoFinal_I_Unidad/Controllers/Ejercicio07Controller.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrabajoFinal_I_Unidad.Models;
+
+namespace TrabajoFinal_I_Unidad.Controllers
+{
+    public class Ejercicio07Controller : Controller
+    {
+        // GET: Ejercicio07
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ObtenerListado()
+        {
+            string Valor = Ejercicio07.ObtenerData();
+            //JObject jsonArray = JObject.Parse(Valor);
+            return Json(new { data = Valor });
+        }
+    }
+}
---
+++ TrabajoFinal_I_Unidad/Models/Ejercicio07.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TrabajoFinal_I_Unidad.Models
+{
+    public class Ejercicio07
+    {
+        public static string ObtenerData()
+        {
+            string strText = "";            
+            using (StreamReader r = new StreamReader(System.Web.HttpContext.Current.Server.MapPath("~/assets/planesEstudio.json")))
+            {
+                strText = r.ReadToEnd();
+            }
+            return strText;
+        }
+    }
+}
